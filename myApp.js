64a28c0cb85a38b78c237d537804914ad9d22d9e
Rodrigo Godo <rodrigogodo@gmail.com>
2022-08-16 20:42:29 -0500

    feat: exercise 3

--- myApp.js
+++ myApp.js
@@ -1,33 +1,10 @@
-let express = require('express');
+import { express } from "./express";
+
+let express = require("express");
 let app = express();
-console.log('Hello World');
-app.get('/', (req, res) => {
-    res.send('Hello Express');
-})
+console.log("Hello World");
+app.get("/", (req, res) => {
+  res.send("/views/index.html");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
